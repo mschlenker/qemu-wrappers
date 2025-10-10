2b1c18c49951147d9bfa0a6d718713facace397c
Mattias Schlenker <mattias.schlenker@checkmk.com>
2025-10-10 17:02:07 +0200

    guess VNC port and tap device

--- quickanylinux_config.sh
+++ quickanylinux_config.sh
@@ -10,19 +10,22 @@
 
 DISKSIZE=32
 
-# General parameters for running, overwrite in quickvirt_config.sh you place into the VM dir.
+# General parameters for running:
 
 CPUS=2
 MEM=4096
 CPU="-cpu host" # For maximum compatibility
-VNC=":23"
+# If you set VNC to an empty string, it will use a random port between 10 and 99
+VNC=""
 DAEMONIZE="-daemonize" # set to empty string to run in foreground
+KEYBOARD="en-us" # Keyboard for VNC
 
 # Networking parameters, simple:
 
 # Default networking requires the dummybridge script being run in advance, thus the vmtap
 # interfaces are available and owned by the user running the script.
-TAPDEV="vmtap1"
+# Set to an empty string to automatically probe devices from vmtap0 to vmtap9
+TAPDEV=""
 # You might specify a MAC address, for example generated with randmac:
 # MAC="b2:d5:18:8d:01:7b"
 # If no MAC is specified, one is created from the name of $TARGETDIR and appended to the config:
